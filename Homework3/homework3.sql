841f1e1e9c4eab2b8df45f0d965562bdf0f1219c
James He <hejames98@gmail.com>
2019-03-30 05:57:41 -0400

    Update homework3.sql

--- Homework3/homework3.sql
+++ Homework3/homework3.sql
@@ -59,28 +59,53 @@ HAVING count(customerid) >= (
 --    quantity is greater than the average order quantity of that product: 
 --    (Hint: This involves a correlated subquery.)
 
--- The following is maybe right:
-
--- SELECT orderid, orderedquantity FROM ORDERLINE_T t
--- WHERE orderedquantity >= (
--- 	SELECT AVG(orderedquantity), productid FROM OrderLine_T
--- 	WHERE productid = t.productid
--- 	GROUP BY productid
--- )
-
+select ol1.orderid, ol1.orderedquantity
+from OrderLine_T ol1
+where orderedquantity > (
+select avg(ol2.orderedquantity)
+from orderline_T ol2
+where ol1.productid = ol2.productid)
 
 -- 9. For each product display in ascending order by product ID the product ID and description 
 --    along with the customer ID and name for the customer who has bought the most of that 
 --    product; also show the total quantity ordered by that customer (who has bought the 
 --    most of that product). Use a correlated subquery:
 
+select ol1.productid, p.ProductDescription, c.CustomerID, c.CustomerName, ol1.OrderedQuantity
+from orderline_t ol1, Product_T p, Order_T o, Customer_T c
+where c.CustomerID = o.CustomerID and o.OrderID = ol1.OrderID and ol1.ProductID = p.ProductID
+
+/*
+Me trying to figure out the sub query  vvvvvvvvvvvvvvv
+
+select sum(ol.orderedquantity), ord.CustomerID
+from OrderLine_T ol, Order_T ord 
+where ord.OrderID = ol.OrderID
+group by CustomerID
+
+select ol.productid, ol.orderedquantity, o.customerid
+from orderline_t ol, Order_T o
+where ol.orderid = o.orderid
+group by customerid
+*/
+
 -- 10. Write a query to get the product(s) whose total order is the lowest in the organization.  
 --     Base the total order on revenue generated (standard price * order quantity).  Outout  
 --     the product id and the revenue for this product.
+
+select p1.ProductID, sum(p1.productstandardprice * ol1.OrderedQuantity) as Revenue
+from Product_T p1, OrderLine_T ol1
+where p1.ProductID = ol1.ProductID
+group by p1.ProductID
+having sum(p1.productstandardprice * ol1.OrderedQuantity) in (
+select top 1 sum(p.productstandardprice * ol.OrderedQuantity)
+from Product_T p, OrderLine_T ol
+where p.ProductID = ol.ProductID
+group by p.ProductID
+order by sum(p.productstandardprice * ol.OrderedQuantity) asc)
 
 -- 11. Find the bottom 3 customers who generated the lowest amount of revenue.  For these 
 --     customers, output the customer name and customer state.  There is a tie for 3rd place,  
 --     so your query needs to be dynamic enough to return all 4 customers who match the  
 --     criteria, without hardcoding anything like “4” in your query.
 
-SELECT o.customerid, p.productid, [orderedquantity] * [productstandardprice] as [Revenue] FROM ORDER_T o, ORDERLINE_T ol, PRODUCT_T p
