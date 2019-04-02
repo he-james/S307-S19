bdad984352b0f8c0ad61f9a0e54e5ae19879951f
James He <hejames98@gmail.com>
2019-04-02 07:59:42 -0400

    Update and rename james's bullshit.sql to HW3-Submission.sql

--- Homework3/HW3-Submission.sql
+++ Homework3/HW3-Submission.sql
@@ -9,19 +9,24 @@ EXCEPT SELECT o.customerid FROM ORDER_T o;
 ----- DOUBLE CHECK THIS ONE
 --    ^^^^^^^^^^^^^^^^^^^^^
 
-SELECT employeename, count(employeesupervisor) as [HeadCount] FROM EMPLOYEE_T
+SELECT employeename, COUNT(employeesupervisor) as [HeadCount] 
+FROM EMPLOYEE_T
 GROUP BY employeename
 HAVING COUNT(employeesupervisor) >= 2;
 
 -- 3. Names of states where customers reside, but have no employees residing in that state:
 
-SELECT customerstate FROM CUSTOMER_T MINUS SELECT employeestate FROM EMPLOYEE_T;
+SELECT customerstate 
+FROM CUSTOMER_T 
+MINUS
+SELECT employeestate 
+FROM EMPLOYEE_T;
 
 -- 4. Display the customer ID, name, and order ID for all customer orders, 
 --    For those customers who do not have any orders, include them in the display once,
 --    with a 0 value for OrderID. 
 
-SELECT c.CustomerID, CustomerName, isnull (o.OrderID,0) as 'OrderID' FROM CUSTOMER_T c 
+SELECT c.CustomerID, CustomerName, ISNULL (o.OrderID,0) as 'OrderID' FROM CUSTOMER_T c 
 LEFT JOIN ORDER_T o on c.customerid = o.customerid;
 
 -- 5. Show the customer ID and name for all the customers who have ordered both products 
@@ -30,14 +35,10 @@ LEFT JOIN ORDER_T o on c.customerid = o.customerid;
 --------------- DOES NOT REMOVE DUPLICATES. unsure if we need to
 ---             ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-SELECT c.CustomerID, 
-	   o.orderid, 
-	   ol.ProductID 
-	   FROM CUSTOMER_T c, 
-	   		ORDER_T o, 
-	   		OrderLine_T ol
+SELECT c.customerid, o.orderid, ol.productid 
+FROM CUSTOMER_T c, ORDER_T o, OrderLine_T ol
 WHERE ol.productid in (5,4) 
-GROUP BY c.CustomerID, o.OrderID, ol.ProductID;
+GROUP BY c.customerid, o.OrderID, ol.productid;
 
 -- 6. List the IDs and names of all products that cost less than the average product price 
 --    in their product line.
@@ -52,42 +53,49 @@ SELECT t.territoryid, territoryname FROM DOESBUSINESSIN_T dbi
 LEFT JOIN TERRITORY_T t ON dbi.territoryid = t.territoryid
 GROUP BY t.territoryid, territoryname
 HAVING count(customerid) >= (
-	SELECT 1.5 * count(customerid) / count(distinct territoryid) FROM DOESBUSINESSIN_T
-);
+	SELECT 1.5 * count(customerid) / count(distinct territoryid) 
+    FROM DOESBUSINESSIN_T);
 
 -- 8. List the order number and order quantity for all customer orders for which the order 
 --    quantity is greater than the average order quantity of that product: 
 --    (Hint: This involves a correlated subquery.)
 
--- The following is maybe right:
-
--- SELECT orderid, orderedquantity FROM ORDERLINE_T t
--- WHERE orderedquantity >= (
--- 	SELECT AVG(orderedquantity), productid FROM OrderLine_T
--- 	WHERE productid = t.productid
--- 	GROUP BY productid
--- )
-
+SELECT ol1.orderid, ol1.orderedquantity
+FROM ORDERLINE_T ol1
+WHERE orderedquantity > (
+SELECT AVG(ol2.orderedquantity)
+FROM ORDERLINE_T ol2
+WHERE ol1.productid = ol2.productid);
 
 -- 9. For each product display in ascending order by product ID the product ID and description 
 --    along with the customer ID and name for the customer who has bought the most of that 
 --    product; also show the total quantity ordered by that customer (who has bought the 
 --    most of that product). Use a correlated subquery:
-select ol.ProductID, p.ProductDescription, c.CustomerID, c.CustomerName, sum(ol.OrderedQuantity) as [TotOrdered]
-from OrderLine_T ol, Customer_T c, Order_T o, Product_T p
-where ol.ProductID = p.ProductID and ol.OrderID = o.OrderID and o.CustomerID = c.CustomerID 
-group by p.ProductDescription, ol.ProductID, c.CustomerID, c.CustomerName, ol.OrderID
-having sum(ol.orderedquantity) in (
-	select sum(ol2.OrderedQuantity)
-	from OrderLine_T ol2
-	where ol.OrderId = ol2.OrderId
-	group by ol2.ProductID) 
-order by ol.ProductID asc
-;
+SELECT ol.ProductID, p.ProductDescription, c.CustomerID, c.CustomerName, SUM(ol.OrderedQuantity) AS [TotOrdered]
+FROM OrderLine_T ol, Customer_T c, Order_T o, Product_T p
+WHERE ol.ProductID = p.ProductID AND ol.OrderID = o.OrderID AND o.CustomerID = c.CustomerID 
+GROUP BY p.ProductDescription, ol.ProductID, c.CustomerID, c.CustomerName, ol.OrderID
+HAVING SUM(ol.orderedquantity) IN (
+	SELECT sum(ol2.OrderedQuantity)
+	FROM OrderLine_T ol2
+	WHERE ol.OrderId = ol2.OrderId
+	GROUP BY ol2.ProductID) 
+ORDER BY ol.ProductID;
 
 -- 10. Write a query to get the product(s) whose total order is the lowest in the organization.  
 --     Base the total order on revenue generated (standard price * order quantity).  Outout  
 --     the product id and the revenue for this product.
+
+SELECT p1.ProductID, sum(p1.productstandardprice * ol1.OrderedQuantity) AS [Revenue]
+FROM Product_T p1, OrderLine_T ol1
+WHERE p1.ProductID = ol1.ProductID
+GROUP BY p1.ProductID
+HAVING sum(p1.productstandardprice * ol1.OrderedQuantity) in (
+    SELECT TOP 1 SUM(p.productstandardprice * ol.OrderedQuantity)
+    FROM Product_T p, OrderLine_T ol
+    WHERE p.ProductID = ol.ProductID
+    GROUP BY p.ProductID
+    ORDER BY SUM(p.productstandardprice * ol.OrderedQuantity));
 
 -- 11. Find the bottom 3 customers who generated the lowest amount of revenue.  For these 
 --     customers, output the customer name and customer state.  There is a tie for 3rd place,  
@@ -103,5 +111,4 @@ HAVING SUM(p.ProductStandardPrice * ol.OrderedQuantity) in (
     FROM PRODUCT_T p, ORDERLINE_T ol, CUSTOMER_T c, ORDER_T o
     WHERE c.customerid = o.customerid and o.orderid = ol.orderid and ol.productid = p.productid
     GROUP BY c.customerid
-    ORDER BY SUM(p.ProductStandardPrice * ol.OrderedQuantity)
-);
+    ORDER BY SUM(p.ProductStandardPrice * ol.OrderedQuantity));
